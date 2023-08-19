817636a8960cf20417c002f2023b4b102f3ff556
jelvas123 <137334054+jelvas123@users.noreply.github.com>
2023-08-19 22:51:36 +0000

    first

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Howdy!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock")do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+  if@comp_move == "rock"
+      @outcome =  "tied"
+  elsif @comp_move == "paper"
+      @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra)
 end
